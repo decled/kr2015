ef0a6036fbb4dac217d2fa3dd82afda214fa6ccf
decled <vladosla@ukr.net>
2015-12-16 06:19:34 +0200

    Update LifeModel.java

--- LifeModel.java
+++ LifeModel.java
@@ -1,7 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.util.Observable;
-impot java.util.Observer;
+import java.util.Observer;
 import java.util.Timer;
 import java.util.TierTask;
 import javax.swing.JPanel;
@@ -10,7 +10,7 @@ public class Lifemodel  extends Jpanel implements Observer{
 static final int Statealive=1; statedeath=0;
 Color Alive,Death;
 JPanel [] [] Cells;
-int gridwidth; 
-int gridhighth;
+int grid_width; 
+int grid_high;
 
 Timer time =  new Timer;
